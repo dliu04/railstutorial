463c7f472ef09c8275e7f43e17c223ab9d56d00f
Dylan Liu <dylan.liu@seeq.com>
2026-07-29 15:41:42 -0700

    Updated: 2026-07-29 15:41:42

--- hello_app/config/routes.rb
+++ hello_app/config/routes.rb
@@ -11,4 +11,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  root "application#hello"
+
+  # Routes {baseUrl}/hello to "application#hello"
+  # get "/hello" => "application#hello"
 end
